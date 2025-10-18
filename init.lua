71d0648b9601cf37ab6e5a054cd4b741562874a6
veprogames <vepro8673@mailbox.org>
2025-10-18 12:56:32 +0200

    init

---
+++ init.lua
@@ -0,0 +1,103 @@
+---@param value string?
+---@return number?
+local function parse(value)
+	if value == nil then return nil end
+
+	local no_chg = string.sub(value, 5)
+
+	return tonumber(no_chg)
+end
+
+---@param tbl table
+---@param path string[]
+---@param value any
+---@return nil
+local function table_set(tbl, path, value)
+	if #path == 1 then
+		tbl[path[1]] = value
+	else
+		if tbl[path[1]] == nil then
+			tbl[path[1]] = {}
+		end
+
+		local first = table.remove(path, 1)
+		table_set(tbl[first], path, value)
+	end
+end
+
+---@class ControlParams
+---@field id string
+---@field prop string
+---@field min number
+---@field max number
+---@field x number
+---@field y number
+
+---@param params ControlParams
+---@return string
+local function control(params)
+	core.register_on_player_receive_fields(function (player, formname, fields)
+		if formname ~= "visuals2:main" then return end
+
+		local value = parse(fields[params.id])
+		if value == nil then return end
+
+		value = params.min + (value / 1000.0 * (params.max - params.min))
+		local lighting = {}
+		table_set(lighting, params.prop:split("."), value)
+		player:set_lighting(lighting)
+	end)
+
+	return string.format([[
+		label[%f,%f;%s]
+		scrollbar[%f,%f;3,0.5;horizontal;%s;0]
+	]], params.x, params.y, params.prop,
+		params.x, params.y + 0.3, params.id)
+end
+
+core.register_globalstep(function ()
+	local player = core.get_player_by_name("singleplayer")
+	if player ~= nil then
+		local controls = player:get_player_control()
+		if controls.aux1 then
+			local form_controls = {
+				control {
+					id = "artificial_light_r",
+					prop = "artificial_light.r",
+					min = 0,
+					max = 1,
+					x = 1,
+					y = 1,
+				},
+				control {
+					id = "artificial_light_g",
+					prop = "artificial_light.g",
+					min = 0,
+					max = 1,
+					x = 4,
+					y = 1,
+				},
+				control {
+					id = "artificial_light_b",
+					prop = "artificial_light.b",
+					min = 0,
+					max = 1,
+					x = 7,
+					y = 1,
+				},
+			}
+
+			local formspec = string.format([[
+				formspec_version[10]
+				size[20,5]
+				%s
+			]], table.concat(form_controls, "\n"))
+
+			core.show_formspec(
+				player:get_player_name(),
+				"visuals2:main",
+				formspec
+			)
+		end
+	end
+end)
